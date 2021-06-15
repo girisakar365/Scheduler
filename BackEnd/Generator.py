0f154dcb1c5c729bfa5ac38fea1b56ad17ba021f
Sakar Giri <sakargiri365@gmail.com>
2021-06-15 21:57:19 +0545

    Algorithum Update

--- BackEnd/Generator.py
+++ BackEnd/Generator.py
@@ -1,4 +1,6 @@
 from random import * 
+from math import log
+from typing import get_origin
 
 class Generator:
 
@@ -32,6 +34,7 @@ class Generator:
 
         self._dcord()
         self.brks()
+        self._lcord()
 
     def _dcord(self):
         dcord=[]
@@ -48,6 +51,34 @@ class Generator:
             self.dcord_dict[self.shift+str(i+1)]=[dcord[i+count] for i in range(self.tp*6)]
             count+=self.tp*6
 
+    def _lcord(self):
+        lcord=[]
+
+        checklist=[i for i in range(self.tp) if i%2!=0]#check list / break list
+        if self.tp%2!=0:
+            checklist.append(self.tp-1)
+
+        for i in range(self.total_section):#making list of [i,list.index(i)+1]
+            
+            for j in self.dcord_dict[self.shift+str(i+1)]:
+                try:
+                    
+                    if self.faculty=='Science' and i>self.optii[0]:
+                        checklist.append(2)
+                    
+                    if j[1] not in checklist:
+                        lcord.append([j , self.dcord_dict[self.shift+str(i+1)]
+                            [self.dcord_dict[self.shift+str(i+1)].index(j)+1]
+                            ])
+
+                except Exception:
+                   continue
+        count=0
+        for i in range( self.total_section ):
+            var=lcord[count : 6 * int(log(self.tp,2))*(i+1)]
+            self.lab_dict[self.shift+str(i+1)]=var
+            count+=6* int(log(self.tp,2))
+        
     def brks(self):
 
         rng=self.total_section
@@ -77,15 +108,14 @@ class Generator:
             self.dcord_dict[sec].pop(self.dcord_dict[sec].index(td))
             return td
 
-a=Generator('Science','D',18,(1,17),8)
-count=0
+    def gen_lab(self,sec):
+        if sec in  [i for i in self.lab_dict.keys()]:
+            shuffle(self.lab_dict[sec])
+            ld=choice(self.lab_dict[sec])
+            
+            self.lab_dict[sec].pop(self.lab_dict[sec].index(ld))
+            for i in ld:
+                self.dcord_dict[sec].pop(self.dcord_dict[sec].index(i))
+            return ld
 
-for i in range(a.total_section):
-    
-    for j in a.dcord_dict[a.shift+str(i+1)]:
-        try:
-            print([j , a.dcord_dict[a.shift+str(i+1)][
-                a.dcord_dict[a.shift+str(i+1)].index(j)+1
-            ]])
-        except Exception:
-           continue
+a=Generator('Science','D',10,(3,14),2)
