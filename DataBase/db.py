37d7e72dd7f29c7d0b9a48b84063b43147841456
Sakar Giri <sakargiri365@gmail.com>
2021-06-18 10:47:51 +0545

    fetch function  modified

--- DataBase/db.py
+++ DataBase/db.py
@@ -13,8 +13,14 @@ class DB:
 		root.execute("INSERT INTO techid VALUES (%s,%s,%s,%s,%s,%s)",data)
 		main_db.commit()
 
-	def fetch():
-		root.execute('SELECT * FROM techid')
+	def fetch(data='*'):
+		if data =='*':
+			root.execute('SELECT * FROM techid')
+		else:
+			root.execute(f'''SELECT * FROM techid 
+			WHERE fname = '{data}' OR sname = '{data}' OR id = '{data}' OR subject = '{data}' OR 
+			email = '{data}' OR class = '{data}'
+			''')
 		return root.fetchall()
 	
 	def delete():
