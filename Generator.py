089510eae8b275feef94eee774b40d5761066889
girisakar365 <70592584+girisakar365@users.noreply.github.com>
2021-07-06 23:11:40 +0545

    Managing data bugs still exists!

--- Generator.py
+++ Generator.py
@@ -1,8 +1,8 @@
 from random import *
-from math import log
+from math import e, log
 from DataBase.main import *
 
-class Generator:
+class Coordinates:
 
     def __init__(self,cls:int,faculty:str,shift:str,
     sections:int,periods:int,days:int,opt:int):
@@ -15,12 +15,10 @@ class Generator:
         self.days=days
         self.opt=opt
         self.brk=(3,4)
-    
+        
         self.row_col={}
 
         self.lab_dict={}
-
-        self.info={}
 
         self.coordinates()
 
@@ -99,3 +97,63 @@ class Generator:
                 count+= self.days * int(log(self.periods,2))
         
         lab_coordinates()
+
+class Data_Center(Coordinates):
+
+    def __init__(self, cls: int,faculty:str,shift:str,sections:int,periods:int,days: int,opt:int):
+        super().__init__(cls, faculty, shift, sections, periods, days, opt)
+
+
+        self.subject={ i:[j[0] for j in DB.fetch(table=i)]#unit of subject 
+        
+        for i in {i[0] for i in DB.fetch(table='Professor',typ='specific',col='Subject')}#subject set
+        }
+
+        self.info={i:[] for i in self.subject}
+
+        self._sections=[i for i in self.row_col]
+
+        self.data={i:[] for i in self.subject}
+
+        self.sort_teacher()
+        self.sort_data()
+
+    def sort_teacher(self):
+        
+        for j in self.subject:
+            for i in DB.fetch('Professor','quiere','id',f'Subject = "{j}" AND Class = "{self.cls}"'):
+                if len(self.info[j])<self.sections:
+                    self.info[j].append(i[0])
+                
+            if len(self.info[j])<self.sections:
+                for i in DB.fetch('Professor','quiere','id',f'Class = "both" AND Subject="{j}"'):
+                    if len(self.info[j])<self.sections:
+                        self.info[j].append(i[0])
+
+    def sort_data(self):
+
+        for i in self.info:
+            TOTAL=len(self.info['English'])
+        
+            while len(self.info['English'])!=0:
+                teacher=choice(self.info['English'])
+                
+                try:
+                    sec=[]
+                    for sec in range( int( round (self.sections/TOTAL ,0) ) ):
+                        
+                        section=choice(self._sections)
+                        self._sections.remove(section)
+                        sec.append(section)
+                
+                        print('English',
+                            teacher,section)
+
+                except Exception:
+                    pass
+
+                self.info['English'].remove(teacher)
+
+domy=Data_Center(11,'Science','Day',18,7,6,2)
+
+# print(domy.data)
