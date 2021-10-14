96a441232c494ae0932ac0bef07a1220183b0be7
Sakar <sakargiri365@gmail.com>
2021-10-14 23:32:32 +0545

    Done with main logic

--- Coordinates.py
+++ Coordinates.py
@@ -1,11 +1,19 @@
 from random import *
 from math import log
 
-class Coordinates:
 
-    def __init__(self,cls:int,faculty:str,shift:str,
-    sections:int,periods:int,days:int,opt:int):
-        
+class Coordinates:
+    def __init__(
+        self,
+        cls: int,
+        faculty: str,
+        shift: str,
+        sections: int,
+        periods: int,
+        days: int,
+        opt: int,
+    ):
+
         self.CLASS = cls
         self.FACULTY = faculty
         self.SHIFT = shift[0]
@@ -13,16 +21,16 @@ class Coordinates:
         self.PERIODS = periods
         self.DAYS = days
         self.OPT = opt
-        self.BREAKS = (3,4)
-        
+        self.BREAKS = (3, 4)
+
         self.row_col = {}
 
         self.lab_dict = {}
 
         self.coordinates()
 
-    def coordinates(self):#manages coordinates 
-        
+    def coordinates(self):  # manages coordinates
+
         if self.BREAKS[0] < self.BREAKS[1]:
             min, max = self.BREAKS[0], self.BREAKS[1]
         else:
@@ -32,36 +40,41 @@ class Coordinates:
             days = []
             count = 0
 
-            gap = ((self.DAYS + 1) * self.OPT) - 1 #puts gap between sections
+            gap = ((self.DAYS + 1) * self.OPT) - 1  # puts gap between sections
 
-            for i in range((self.DAYS + 1) * self.SECTIONS):#rows 
-                
+            for i in range((self.DAYS + 1) * self.SECTIONS):  # rows
+
                 if i != (self.DAYS + 1) * count:
 
-                    for j in range(self.PERIODS):#columns
-                        
+                    for j in range(self.PERIODS):  # columns
+
                         if self.PERIODS >= min:
 
-                            if i <= gap and j != max or i > gap and j != min:#manage breaks from data base remember to sort them
+                            if (i <= gap and j != max or i > gap and j != min):  
+                            # manage breaks from data base remember to sort them
                                 days.append((i, j))
 
-                        else:days.append((i, j))
+                        else:
+                            days.append((i, j))
 
-                else: count += 1
-            
+                else:
+                    count += 1
+
             return days
 
         def sort_coordintes():
-            count=0
-            
-            days=gen_coordinates()
+            count = 0
 
-            if self.PERIODS > min: end = (self.DAYS * (self.PERIODS - 1))
-            
-            else: end = (self.DAYS * (self.PERIODS))
+            days = gen_coordinates()
 
-            for i in range(self.SECTIONS): 
-                self.row_col[self.SHIFT + str(i + 1)] = days[count : end * (i + 1)]      
+            if self.PERIODS > min:
+                end = self.DAYS * (self.PERIODS - 1)
+
+            else:
+                end = self.DAYS * (self.PERIODS)
+
+            for i in range(self.SECTIONS):
+                self.row_col[self.SHIFT + str(i + 1)] = days[count : end * (i + 1)]
                 count += end
 
         sort_coordintes()
@@ -69,28 +82,35 @@ class Coordinates:
         def lab_coordinates():
             lcord = []
 
-            checklist = [i for i in range(self.PERIODS) if i % 2 != 0]#check list / break list
-            
-            if self.PERIODS % 2 != 0: checklist.append(self.PERIODS - 1)
+            checklist = [
+                i for i in range(self.PERIODS) if i % 2 != 0
+            ]  # check list / break list
 
-            for i in range(self.SECTIONS):#making list of [i,list.index(i)+1]
-                
+            if self.PERIODS % 2 != 0:
+                checklist.append(self.PERIODS - 1)
+
+            for i in range(self.SECTIONS):  # making list of [i,list.index(i)+1]
+
                 for j in self.row_col[self.SHIFT + str(i + 1)]:
                     try:
-                        
-                        if self.faculty == 'Science' and i > min: checklist.append(2)
-                        
+
+                        if self.faculty == "Science" and i > min:
+                            checklist.append(2)
+
                         if j[1] not in checklist:
-                            lcord.append([j, self.row_col[self.SHIFT + str(i + 1)]
-                                [self.row_col[self.SHIFT + str(i + 1)].index(j) + 1]
-                                ])
+                            lcord.append(
+                                [j, self.row_col[self.SHIFT + str(i + 1)][
+                                    self.row_col[self.SHIFT + str(i + 1)].index(j) + 1
+                                    ],
+                                ]
+                            )
 
                     except Exception:
-                       continue
+                        continue
             count = 0
             for i in range(self.SECTIONS):
                 var = lcord[count : self.DAYS * int(log(self.PERIODS, 2)) * (i + 1)]
                 self.lab_dict[self.SHIFT + str(i + 1)] = var
                 count += self.DAYS * int(log(self.PERIODS, 2))
-        
+
         lab_coordinates()
--- Generator.py
+++ Generator.py
@@ -1,35 +1,124 @@
 from random import *
 from Allocation import Allocate
+from database import *
 
 # NOTE: Algorithum does not hold validation for less than five classes.
 
 class Generator(Allocate):
-
-    def __init__(self, cls: int,faculty:str,shift:str,sections:int,periods:int,days: int,opt:int):
+    def __init__(
+        self,
+        cls: int,
+        faculty: str,
+        shift: str,
+        sections: int,
+        periods: int,
+        days: int,
+        opt: int,
+    ):
         super().__init__(cls, faculty, shift, sections, periods, days, opt)
 
-        self.__iter__()
+        self.time_per_subject = {
+            next(iter(unit)): None
+            for subject in self.subject for unit in DB.fetch(f'"{subject}"', 'specific', 'unit')
+            }
 
-    def __iter__(self):
+        for subject in self.subject:
+            for key in self.time_per_subject: 
+                for j in DB.fetch(f'"{subject}"', 'quiere', 'tpp', f'unit = "{key}"'): self.time_per_subject[key] = next(iter(j))
+
+
+        self.sorted_cord = {slot: {section: None
+        for j in self.subject_section[slot]
+        for section in j}
+        for slot in self.subject_section}
+
+        for slot in self.subject_section: 
+            for sec in self.subject_section[slot]: self.filter(slot, sec)
+
+    def corr_rows(self, row: int, sec_no):
+        unit_row = row - ((self.DAYS + 1) * (sec_no - 1))
         
-        for i in self.subject_section:
+        return [( (self.DAYS + 1) * i) + unit_row for i in range(self.SECTIONS)]
 
-            for j in self.subject_section[i]:
-                print(i)
-                self.__next__(j)
+    def filter(self, slot: str, sections: list):
 
-    def __next__(self, sections: list):
+        def filter_for_corresponding_row(): 
+            section_list = []
+
+            for section in sections:
+                section_list.append(sample(self.row_col[section], self.time_per_subject[slot]))
+
+            for index in range(len(section_list)):
+
+                sec_no = int(sections[index][1:]) #section no.
+
+                for cord in section_list[index]:
+                    corr_row = self.corr_rows(cord[0], sec_no) #taking corresponding rows
+
+                    for index_ in range(len(section_list)):
+                        if index != index_ and index_ > index: #index 1 != index 2 and pervious index must not repate
+
+                            for cord_ in section_list[index_]:
+
+                                if cord_[1] == cord[1] and cord_[0] in corr_row: 
+                                    # if col of index1 == col of index2 and row of index2 in corr_row
+                                    conflict = cord_
+
+                                    while conflict == cord_: #generate cord of non-correspoding rows
+                                        
+                                        conflict = sample(self.row_col[sections[index_]], self.time_per_subject[slot])[0]
+                                        if conflict[0] in corr_row: conflict = cord_ # check if non-correspoding row; else repate
+
+                                        else: # else change the pervious cord with cord with non-corresponding row
+                                            k = section_list[index_].index(cord_) 
+                                            section_list[index_][k] = conflict
+
+            for i in range(len(sections)):
+                self.sorted_cord[slot][sections[i]] = filter_for_same_row(sections[i], section_list[i])
         
-        coordintes = []
-
-        avoide_rows = None
+        def filter_for_same_row(section, section_list): #Check if row are equal for same section.
+            
+            sec_no = int(section[1:])
         
-        condition = True
+            for i in section_list:
+                
+                conflict_list = {i[0] for i in section_list} # contains unique rows
+                
+                for j in section_list:
+                
+                    if i[0] == j[0] and i != j or i == j and section_list.count(i) > 1: 
+                    # Check if row_1 == row_2 for different cord | if number of same cord in list > 1
 
-        for i in sections: 
+                        conflict = j
+                        count = 0
 
-            print(choice( self.row_col[i] ))
+                        while conflict == j :
+                            count += 1
+                            
+                            conflict = sample(self.row_col[section], self.time_per_subject[slot])[0]
+                            if conflict[0] in conflict_list: conflict = j 
+                            # checks if new row generated is already in the set of unique rows or not
+                            # if true: value of conflict remains unchanged
 
-domy_data = (11, 'Science', 'Day', 18, 7, 6, 2)
+                            if count > 100: #if coordinates are less and no options are left for new generation: try for 100; break loop
+                                break
 
-domy = Generator(*domy_data)
+                            else: 
+                                section_list[section_list.index(j)] = conflict # value of conflict is moved to the section_list
+                                conflict_list.add(conflict[0]) # adds unique row generated to slove conflict to conflict_list
+            
+            for section_removed in section_list: 
+
+                try:
+                    self.row_col[section].pop(
+                    self.row_col[section].index(section_removed)
+                )
+
+                except ValueError:
+                    pass
+
+            return section_list
+
+        filter_for_corresponding_row()
+
+# domy = {'cls': 11, 'faculty': 'Science', 'shift': 'Day', 'sections': 18, 'periods': 7, 'days': 6, 'opt': 7}
