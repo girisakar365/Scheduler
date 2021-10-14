5dc869d48edd67b7b91ab06e880b96fafd608019
Sakar <sakargiri365@gmail.com>
2021-10-14 23:33:28 +0545

    code formating

--- database.py
+++ database.py
@@ -1,32 +1,35 @@
 from sqlite3 import *
 
+
 class DB:
-	conn=connect('3ZH9iusR.db')
-	cur=conn.cursor()
+    conn = connect("3ZH9iusR.db")
+    cur = conn.cursor()
 
-	def create(subject):
-		DB.cur.execute(f"CREATE TABLE IF NOT EXISTS {subject} (unit TEXT );")
-		DB.conn.commit()
+    def create(subject):
+        DB.cur.execute(f"CREATE TABLE IF NOT EXISTS {subject} (unit TEXT, tpp INTEGER);")
+        DB.conn.commit()
 
-	def insert(data,table='Professor'):
-		if table == 'Professor': 
-			try: DB.cur.execute(f"INSERT INTO {table} VALUES ({'?,?,?,?,?,?'});", data)
+    def insert(data, table="Professor"):
+        if table == "Professor":
+            try:
+                DB.cur.execute(f"INSERT INTO {table} VALUES ({'?,?,?,?,?,?'});", data)
 
-			except Exception: pass
-			
-		else: DB.cur.execute(f"INSERT INTO {table} VALUES ({'?'});", [data])
-		
-		DB.conn.commit()
+            except Exception:
+                pass
 
+        else:
+            DB.cur.execute(f"INSERT INTO {table} VALUES ({'?'});", [data])
 
-	def fetch(table:str='Professor',typ:str='normal',col:str=None,*arg):
+        DB.conn.commit()
 
-		if typ=='normal':
-			cmd=f'SELECT * FROM {table}'#fetch all 
-		elif typ=='specific':
-			cmd=f'SELECT {col} FROM {table}'#fetch specific col
-		elif typ=='quiere':#fetch comparing data's 
-			cmd=f'SELECT {col} FROM {table} WHERE {arg[0]}'
+    def fetch(table: str = "Professor", typ: str = "normal", col: str = None, *arg):
 
-		data=DB.cur.execute(cmd)
-		return data.fetchall()
+        if typ == "normal":
+            cmd = f"SELECT * FROM {table}"  # fetch all
+        elif typ == "specific":
+            cmd = f"SELECT {col} FROM {table}"  # fetch specific col
+        elif typ == "quiere":  # fetch comparing data's
+            cmd = f"SELECT {col} FROM {table} WHERE {arg[0]}"
+
+        data = DB.cur.execute(cmd)
+        return data.fetchall()
