289cbcc794948003958326e3a110f23efc6304d5
Sakar <sakargiri365@gmail.com>
2021-10-25 21:04:19 +0545

    commands updated, methods added to Cache

--- raw.py
+++ raw.py
@@ -6,7 +6,7 @@ class PhotoLib:
 	cur=conn.cursor()
 
 	def insert(img,oid=None,cmd='ins'):
-		with open(f'Image/{img}.png','rb') as pic:
+		with open(f'FrontEnd/Image/{img}.png','rb') as pic:
 			if cmd=='ins':
 				PhotoLib.cur.execute("INSERT INTO plib(img) VALUES (?)",[Binary(pic.read())])
 				PhotoLib.conn.commit()
@@ -23,16 +23,20 @@ class Cache:
 	conn=connect('db.db')
 	cur=conn.cursor()
 
-	def insert(data):
+	def cache(data):
 		try: #Managing unique string error
 			Cache.cur.execute("INSERT INTO cache(sq) VALUES (?)",[data])
 			Cache.conn.commit()
 		except Exception:
 			pass
 
-	def fetch():
-		data=Cache.cur.execute('SELECT * FROM cache')
-		return data.fetchall()
+	def switch(col, data):
+			Cache.cur.execute(f'UPDATE switch SET {col} = (?)',[data])
+			Cache.conn.commit()
+
+	def fetch(table,col):
+		data=Cache.cur.execute(f'SELECT {col} FROM {table}')
+		for data in data.fetchall(): return next(iter(data))
 
 	def update(col,data):
 		data=Cache.cur.execute(f'UPDATE mgSub SET {col} = (?)',[data])
