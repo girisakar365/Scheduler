e838cb013a52d3eb6f3e0753d864cf74686086e3
Sakar <sakargiri365@gmail.com>
2021-12-04 17:20:53 +0545

    Table class added

---
+++ Table.py
@@ -0,0 +1,17 @@
+from src import *
+
+class Table:
+
+    def __init__(self, master, x:int, y:int, width:int, height:int):
+        self.Table = self.table(master, x, y, width, height)
+
+    def table(self, master, x, y, width, height):
+        _table = QTableWidget(master)
+        _table.setStyleSheet(TABLE)
+        _table.move(x, y)
+        _table.resize(width, height)
+        _table.hide()
+
+        return _table
+
+    def row_and_cols(self):pass
